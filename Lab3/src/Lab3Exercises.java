e8f7076b87664de1f672685db6f0d62b8f850d9a
4015c7 <4015c7@icaco.ece.uprm.edu>
2017-02-08 12:25:21 -0400

    Changed the window location, the name and size.

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -4,6 +4,8 @@ public class Lab3Exercises {
 	public static void main(String[] args) {
 		JFrame myFrame = new JFrame("Adahid");
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		
+		// setting the location relative to null will put the window in the center by default, regardless of the screen size//
 		myFrame.setLocationRelativeTo(null);
 		myFrame.setSize(200, 200);
 		myFrame.setVisible(true);
