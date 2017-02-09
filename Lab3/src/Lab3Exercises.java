33124248c1042a15d65bce9e72b94ec7c6dc5d1f
adahidgalan <adahid.galan@upr.edu>
2017-02-08 22:30:44 -0400

    Created Polygons and relocated ovals

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -1,3 +1,7 @@
+import java.awt.Dimension;
+import java.awt.HeadlessException;
+import java.awt.Toolkit;
+
 import javax.swing.JFrame;
 
 public class Lab3Exercises {
@@ -5,12 +9,18 @@ public class Lab3Exercises {
 		JFrame myFrame = new JFrame("Adahid");
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		
-		// setting the location relative to null will put the window in the center by default, regardless of the screen size//
-		myFrame.setLocationRelativeTo(null);
+		//Method to find Dimension of the screen and store it as a coordinate int.
+		Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
+	    int centerX = (int) ((dimension.getWidth() - myFrame.getWidth()) / 2);
+	    int centerY = (int) ((dimension.getHeight() - myFrame.getHeight()) / 2);
+		
+		myFrame.setLocation(centerX -100, centerY -100);
 		myFrame.setSize(200, 200);
 		myFrame.setVisible(true);
 		
 		MyPanelClass myPanel = new MyPanelClass();
         myFrame.getContentPane().add(myPanel);
 	}
-}
+	
+
+	}
